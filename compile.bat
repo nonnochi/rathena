c819ca051605296b22dd8ebe95a0d432bca237b7
null <muramaza.prosever@live.com>
2022-04-17 11:40:22 +0700

    Revert "Merge https://github.com/mundussan/rathena into feature/webservice"
    This reverts commit 0deca6b535d3d1a490317da2dc0a09b97ee095eb, reversing
    changes made to 24b31ed2b13260d8c2919a7b40e1872dc8beab06.

--- compile.bat
+++
@@ -1,7 +0,0 @@
-@Echo OFF
-Echo "Building solution/project file using batch file"
-SET SolutionPath=%~dp0\rAthena.sln
-Echo Start Time - %Time%
-MSbuild.exe %SolutionPath% /p:PlatformToolset=v142 /m /p:Configuration=Release /p:Platform=x64
-Echo End Time - %Time%
-Set /p Wait=Build Process Completed...
--- serverupdate.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-ECHO Getting the latest Git from the upstream
-Echo Wait...
-git pull upstream master
-Echo ---------------------------------------------------
-Set /p Wait="Finished."
