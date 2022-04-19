961ae527d2c60e44d9f2356f51fdd50b57cd4437
null <muramaza.prosever@live.com>
2022-04-19 20:45:22 +0700

    Revert "scheduled commit"
    This reverts commit b961494fee9f33b6ec14404c4ee1f2d2e6313e38.

--- serverupdate - Copy.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-ECHO Getting the latest Git from the upstream
-Echo Wait...
-git add --all
-git diff-index --quiet HEAD || git commit -m "scheduled commit"
-git push
-Echo ---------------------------------------------------
-Set /p Wait="Finished."
