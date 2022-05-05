c8c9c8ce96ba94ced756ae713dcfba234b4befbf
null <muramaza.prosever@live.com>
2022-05-05 22:49:00 +0700

    Update serverupdate.bat

--- serverupdate.bat
+++ serverupdate.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-ECHO Getting the latest Git from the upstream
+ECHO Getting the latest Git from the null webservice
 Echo Wait...
-git pull upstream master
+git pull https://github.com/il3ol2ed/rathena_webservice.git
 Echo ---------------------------------------------------
 Set /p Wait="Finished."
